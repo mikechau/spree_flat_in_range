bc85e0d9221a3fa09992645d124a7cf8f1150262
Mike Chau <mchau@iwu.edu>
2013-05-26 01:25:47 -0500

    Add free shipping option

--- app/models/spree/calculator/flat_in_range.rb
+++ app/models/spree/calculator/flat_in_range.rb
@@ -4,7 +4,9 @@ module Spree
     preference :upper_boundry,    :decimal, :default => 50.0
     preference :amount,           :decimal, :default => 5.0
     preference :alternative_amount, :decimal, :default => 10.0
-    attr_accessible :preferred_lower_boundry, :preferred_upper_boundry, :preferred_amount, :preferred_alternative_amount
+    preference :free_shipping, :boolean, :default => false
+    preference :free_shipping_price, :decimal, :default => 0.0
+    attr_accessible :preferred_lower_boundry, :preferred_upper_boundry, :preferred_amount, :preferred_alternative_amount, :preferred_free_shipping, :preferred_free_shipping_price
     def self.description
       I18n.t(:flat_in_range)
     end
@@ -13,9 +15,17 @@ module Spree
       sum = 0
       item_total = object.line_items.map(&:amount).sum
       if (item_total >= self.preferred_lower_boundry && item_total <= self.preferred_upper_boundry)
-        return self.preferred_amount
+        if self.preferred_free_shipping == true && self.preferred_free_shipping_price <= item_total
+          return 0.0
+        else
+          return self.preferred_amount
+        end
       else
-        return self.preferred_alternative_amount
+        if self.preferred_free_shipping == true && self.preferred_free_shipping_price <= item_total
+          return 0.0
+        else
+          return self.preferred_alternative_amount
+        end
       end
     end
   end
