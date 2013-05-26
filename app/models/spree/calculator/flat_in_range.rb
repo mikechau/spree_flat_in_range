f6525f0d5e62022a67f78fd6bbfd07e8e92f1225
Mike Chau <mchau@iwu.edu>
2013-05-25 22:48:30 -0500

    Add alternative amount, instead of returning 0

--- app/models/spree/calculator/flat_in_range.rb
+++ app/models/spree/calculator/flat_in_range.rb
@@ -3,7 +3,8 @@ module Spree
     preference :lower_boundry,    :decimal, :default => 0.0
     preference :upper_boundry,    :decimal, :default => 50.0
     preference :amount,           :decimal, :default => 5.0
-    attr_accessible :preferred_lower_boundry, :preferred_upper_boundry, :preferred_amount
+    preference :alternative_amount, :decimal, :default => 10.0
+    attr_accessible :preferred_lower_boundry, :preferred_upper_boundry, :preferred_amount, :preferred_alternative_amount
     def self.description
       I18n.t(:flat_in_range)
     end
@@ -14,7 +15,7 @@ module Spree
       if (item_total >= self.preferred_lower_boundry && item_total <= self.preferred_upper_boundry)
         return self.preferred_amount
       else
-        return 0.0
+        return self.preferred_alternative_amount
       end
     end
   end
